d594c800efb5c980f6a01fca5f96489895c4de37
jeongyuneo <piq2255@gmail.com>
2021-08-29 22:22:51 +0900

    fix: 공백 입력 처리

--- week6/src/main/java/com/livenow/week6/service/Splitter.java
+++ week6/src/main/java/com/livenow/week6/service/Splitter.java
@@ -5,7 +5,6 @@ import com.livenow.week6.domain.Number;
 import com.livenow.week6.domain.Numbers;
 
 import java.util.Arrays;
-import java.util.List;
 import java.util.stream.Collectors;
 
 public class Splitter {
@@ -19,13 +18,20 @@ public class Splitter {
     }
 
     public static Numbers split(String formula) {
+        if (formula.isEmpty()) {
+            return createZeroList();
+        }
         if (formula.contains(SEPARATOR_START) && formula.contains(SEPARATOR_END)) {
             delimiter = DelimiterExtractor.creatCustomDelimiter(formula).getValue();
             formula = formula.substring(5);
         }
-        List<Number> numbers = Arrays.stream(formula.split(delimiter))
+        return new Numbers(Arrays.stream(formula.split(delimiter))
                 .map(Number::new)
-                .collect(Collectors.toList());
-        return new Numbers(numbers);
+                .collect(Collectors.toList()));
+    }
+
+    private static Numbers createZeroList() {
+        return new Numbers(Arrays.stream(new Number[]{new Number("0")})
+                .collect(Collectors.toList()));
     }
 }
